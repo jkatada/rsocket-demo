fadc46cfac7052640f870b4a9bf0fda49f0712a6
KATADA Junya <jkatada@despina.local>
2018-10-02 10:32:06 +0900

    Initial commit

---
+++ src/test/java/com/example/rsocketdemo/RsocketDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.rsocketdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RsocketDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
